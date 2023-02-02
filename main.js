64afcfd8bb06cccc2849949f4b665a789a3e3eed
ssilhun <106236212+ssilhun@users.noreply.github.com>
2023-02-02 16:05:57 +1030

    JS: Navbar menu toggle + Project UI tweak

--- main.js
+++ main.js
@@ -14,3 +14,30 @@ document.addEventListener('scroll', () => {
     }
 
 });
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    navbarMenu.classList.remove('open');
+    scrollIntoView(link);
+});
+
+// Navbar toggle button for smaal screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+function logItem(e) {
+    const item = document.querySelector(`[data-id=${toggle}]`);
+    item.toggleAttribute('hidden');
+}
+
+const chapters = document.querySelectorAll('#toggle');
+chapters.forEach((chapter) => {
+chapter.addEventListener('toggle', logItem);
+});
